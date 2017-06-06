6b80c9afab86af306d2fae6aaba726429f402934
BFunk <bret.e.funk@gmail.com>
2017-06-06 09:32:57 -0600

    working pantry file and test file

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,35 @@
 class Pantry
 
+  attr_accessor :stock
+
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(item)
+    stock[item].nil? ? 0 : stock[item]
+  end
+
+  def restock(item, quantity)
+    stock[item] ||= 0
+    stock[item] += quantity
+  end
 end
+
+# pantry = Pantry.new
+# # => <Pantry...>
+#
+# # Checking and adding stock
+# pantry.stock
+# # => {}
+#
+# pantry.stock_check("Cheese")
+# # => 0
+#
+# pantry.restock("Cheese", 10)
+# pantry.stock_check("Cheese")
+# # => 10
+#
+# pantry.restock("Cheese", 20)
+# pantry.stock_check("Cheese")
+# # => 30
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,66 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  attr_reader :pantry
+
+  def setup
+    @pantry = Pantry.new
+  end
+
+  def test_class_exists
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_return_stock
+    result = pantry.stock
+    pantry_hash = {}
+
+    assert_equal pantry_hash, result
+  end
+
+  def test_stock_check_soylant
+    result = pantry.stock_check("Soylant")
+
+    assert_equal 0, result
+  end
+
+  def test_stock_check_cheese
+    result = pantry.stock_check("Cheese")
+
+    assert_equal 0, result
+  end
+
+  def test_restock_cheese
+    pantry.restock("Cheese", 20)
+    result = pantry.stock_check("Cheese")
+
+    assert_equal 20, result
+  end
+
+  def test_restock_more_cheese
+    pantry.restock("Cheese", 50)
+    result = pantry.stock_check("Cheese")
+
+    assert_equal 50, result
+  end
+
+  def test_restocking_lots_of_cheese
+    pantry.restock("Cheese", 5)
+    pantry.restock("Cheese", 2)
+    pantry.restock("Cheese", 1)
+    result = pantry.stock_check("Cheese")
+
+    assert_equal 8, result
+
+  end
+
+  def test_restocking_soylent
+    pantry.restock("Soylant", 12)
+    pantry.restock("Soylant", 12)
+    pantry.restock("Soylant", 12)
+    result = pantry.stock_check("Soylant")
+
+    assert_equal 36, result
+  end
+
 end
